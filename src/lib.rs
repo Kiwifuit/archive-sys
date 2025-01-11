17c2e16b10eb75361cd6bd5e9ce131b9ecc1149a
Kiwifuit <mahkiwi123@gmail.com>
2025-01-11 10:12:01 +0800

    chore: docs :/

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+//! # Unsafe C Bindings for libarchive
+//!
+//! This crate does not have documentation.
+//! Please refer to the [`libarchive` wiki](https://github.com/libarchive/libarchive/wiki)
+//! for information on how to use this crate.
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
